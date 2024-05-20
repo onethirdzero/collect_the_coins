61e01af3972017307392960d3c8d6ec6e1d8a261
Jordan Siaw <jordansiaw.cy@gmail.com>
2024-05-20 14:42:47 -0700

    Add initial Main scene and script
    - Instantiate Coins dynamically

---
+++ main.gd
@@ -0,0 +1,40 @@
+extends Node
+
+var score
+@export var coin_scene: PackedScene # This lets us instantiate Coins dynamically.
+
+
+func _ready():
+	pass
+
+func game_over():
+	$CoinTimer.stop()
+	
+	print("GAME OVER")
+	# TODO: Display game over message.
+
+
+func new_game():
+	score = 0
+	$Player.start($StartPosition.position)
+	$StartTimer.start()
+	
+	# TODO: Clear previous coins.
+
+
+func _on_start_timer_timeout():
+	$GameTimer.start()
+	$CoinTimer.start()
+
+
+func _on_coin_timer_timeout():
+	print("instantiate coin")
+	var coin = coin_scene.instantiate()
+	
+	# Choose a random location on Path2D.
+	var coin_spawn_location = $CoinPath/CoinSpawnLocation
+	coin_spawn_location.progress_ratio = randf()
+
+	coin.position = coin_spawn_location.position
+	
+	add_child(coin)
