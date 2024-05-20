b66cc64247ee8f72f8b252e8a048002995e6e4f7
Jordan Siaw <jordansiaw.cy@gmail.com>
2024-05-20 15:17:04 -0700

    Add initial HUD scene and script
    - Import font from Godot tutorial:
      https://docs.godotengine.org/en/stable/getting_started/first_2d_game/index.html

---
+++ hud.gd
@@ -0,0 +1,35 @@
+extends CanvasLayer
+
+# Notify Main scene that the game has started.
+signal start_game
+
+func show_message(text):
+	$Message.text = text
+	$Message.show()
+	$MessageTimer.start()
+
+
+func show_game_over():
+	show_message("Game Over")
+	await $MessageTimer.timeout
+
+	# No need to time out this message because this is the title text.
+	$Message.text = "Collect The Coins"
+	$Message.show()
+	
+	# Add a slight delay before showing the start button.
+	await get_tree().create_timer(1.0).timeout
+	$StartButton.show()
+
+
+func update_score(score):
+	$ScoreLabel.text = str(score)
+
+
+func _on_start_button_pressed():
+	$StartButton.hide()
+	start_game.emit()
+
+
+func _on_message_timer_timeout():
+	$Message.hide()
